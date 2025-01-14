52167ee5ecd626a62a21506de6474f433fdbd9ef
Fungai023 <pmambambo023@student.wethinkcode.co.za>
2025-01-14 13:36:06 +0200

    day one complete

---
+++ Day 1/task/task.py
@@ -0,0 +1,9 @@
+#greeting for my program
+print("\t\tBAND NAME GENERATOR\n")
+
+city = input("Which city did you grow up in?\n")
+pet_name = input("What is your pet's name?\n")
+
+print("\nYour band name is : "+ city +"\t"+ pet_name)
+
+
