58ad8f708c89b025de2fc2af8ec83b9991da69b6
Fungai023 <pmambambo023@student.wethinkcode.co.za>
2025-01-16 03:27:35 +0200

    Day nine complete

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -3,4 +3,32 @@
 # TODO-3: Whether if new bids need to be added
 # TODO-4: Compare bids in dictionary
 
+def user_information(bidders):
+
+    name = input("What is your name ? : \n ")
+    amount = input("Bid amount in ZAR : \n ")
+    bidders[name] = amount
+    return input("Are you the last person ? : ")
+
+
+def winner(my_dict):
+    return max(my_dict.values())
+
+
+
+my_bidders = {}
+more_people = user_information(my_bidders)
+
+while more_people.lower() != "yes":
+    print("\n"*100)
+    more_people = user_information(my_bidders)
+else:
+    print("The highest bidder is : R" + winner(
+        my_bidders
+    ))
+
+
+
+
+
 
--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,1 +1,19 @@
+from traceback import print_tb
+
 programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+
+for key in programming_dictionary:
+    print(key)
+
+for key in programming_dictionary:
+    print(programming_dictionary[key])
+
+my_dict = {}
+my_dict["Apples"] =  5
+print(my_dict)
+
+# fastest way to create a dictionary and assigning data to it
+my_farm =  dict(sheep = 7 , cows = 5 , dogs= 20 , lamb = 34)
+print(len(my_farm))
+print(my_farm)
+
