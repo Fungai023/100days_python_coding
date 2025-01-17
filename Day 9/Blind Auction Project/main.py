a839148784f0f9f48035c3a5eaedd4e60b439da7
Fungai023 <pmambambo023@student.wethinkcode.co.za>
2025-01-17 14:44:12 +0200

    completed Day 9 and added all relevent validation

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -1,14 +1,28 @@
 import art
-
+import sys
 
 # TODO-1: Ask the user for input
 # TODO-2: Save data into dictionary {name: price}
 # TODO-3: Whether if new bids need to be added
 def user_information(bidders):
-
-    name = input("What is your name ? : \n ")
-    amount = int(input("Bid amount in ZAR : \n "))
+    name = username()
+    amount = bid_amount()
     bidders[name] = amount
+
+def username():
+    name = input("What is your name ? : \n ")
+    while not name.isalpha() or len(name)==0 :
+        print("Invalid name , please try again","red" )
+        name = input("What is your name ? : \n ")
+    return  name
+
+def bid_amount():
+    amount = int(input("Bid amount in ZAR : \n "))
+    while not type(amount)!= int and len(str(amount)) == 0 :
+        print("Invalid bid amount, please try again .")
+        amount = int(input("Bid amount in ZAR : \n "))
+    return amount
+
 
 def is_last_person():
     is_last = input("Are you the last person ?(yes/no) : ")
@@ -28,12 +42,13 @@ def main():
     user_information(my_bidders)
     more_people = (is_last_person())
     while more_people.lower() != "yes":
-        print("\n"*990)
+        print("\n"*90)
         user_information(my_bidders)
         more_people = is_last_person()
     else:
         win = winner(my_bidders)
         print(f'The highest bidder is {[x for x,y in my_bidders.items() if y == win][0]} : R {win}')
+        print("Item successfully sold .")
 
 if __name__ == "__main__":
     main()
