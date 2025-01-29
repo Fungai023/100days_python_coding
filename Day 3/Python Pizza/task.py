fe0c31ebd96ad26cd6d21fb9f76013c8efd4cba4
Fungai023 <pmambambo023@student.wethinkcode.co.za>
2025-01-29 16:37:26 +0200

    Day 3 Python pizza complete and dates passing

--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -3,3 +3,22 @@ size = input("What size pizza do you want? S, M or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
 
+prices = {
+    'S' : 15,
+    'M': 20,
+    'L': 25,
+}
+
+total = 0
+
+total += prices[size]
+if pepperoni.capitalize() == "Y" :
+    if size.upper() == "S":
+        total+= 2
+    else:
+        total+=3
+elif extra_cheese.upper() == "Y":
+    total+=1
+
+print(f"Your final bill is: ${total}.")
+
