bb42f1a3ffbaeeba0f8e8cd1dd2e7cf2917d868f
Fungai023 <pmambambo023@student.wethinkcode.co.za>
2025-01-31 13:48:30 +0200

    added more scenarioes

--- Day 3/Treasure Island Project/task.py
+++ Day 3/Treasure Island Project/task.py
@@ -9,7 +9,31 @@ cross_road = input("Your at a cross road . \n\t\t Choose Left or Right\n").lower
 
 if cross_road in ["left","l"]:
     print(logo.fort)
-    fort = input("You have reached the enemy fort  . Invade or Run ")
+    fort = input("You have reached the enemy fort  . Invade or Run \n").lower()
+    if fort == "invade":
+        print("You were captured and tortured to death\n Game over , Try again Later ")
+
+    elif fort == "run":
+        run = input("You find yourself in a dense forest. Ahead, you see a dark cave and a narrow wooden bridge over a river. "
+                 "Do you enter the 'cave' or cross the 'bridge'?\n").lower()
+
+        if run == "cave":
+            cave = input("Inside the cave, you discover an ancient temple with three doors, each marked with a strange symbol: "
+                    "a serpent, a sun, and a skull. "
+                    "Which door do you enter? ('serpent', 'sun', or 'skull')\n").lower()
+
+            if cave == "sun":
+                # print(logo.fountain)
+                drink = input("Beyond the door, you find a glowing fountain filled with shimmering water. "
+                              "A stone tablet reads: 'To unlock the path, drink from the right cup.' "
+                              "There are three cups: gold, silver, and clay. "
+                              "Which one do you choose?\n").lower()
+
+            elif cave == "skull":
+                print("You got mummy-fied .Game over")
+
+            elif cave == "serpent" or cave not in ['serpent', 'sun','skull']:
+                print("You bitten by poisoned by toxic fumes\n Game over .")
 
 elif cross_road in ['right','r']:
     lake = input('You\'ve come to a lake. '
