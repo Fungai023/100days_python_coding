1e718568e420d173ae3fc83283836f489d0d1691
Fungai023 <pmambambo023@student.wethinkcode.co.za>
2025-02-03 16:38:09 +0200

    Day 4 complete

--- Day 3/Treasure Island Project/task.py
+++ Day 3/Treasure Island Project/task.py
@@ -1,24 +1,38 @@
-print(r'''
-*******************************************************************************
-          |                   |                  |                     |
- _________|________________.=""_;=.______________|_____________________|_______
-|                   |  ,-"_,=""     `"=.|                  |
-|___________________|__"=._o`"-._        `"=.______________|___________________
-          |                `"=._o`"=._      _`"=._                     |
- _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
-|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
-|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
-          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
- _________|___________| ;`-.o`"=._; ." ` '`."\ ` . "-._ /_______________|_______
-|                   | |o ;    `"-.o`"=._``  '` " ,__.--o;   |
-|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
-____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
-/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
-____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
-/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
-____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
-/______/______/______/______/______/______/______/______/______/______/_____ /
-*******************************************************************************
-''')
+import game_logo as logo
+
+print(logo.game)
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
+
+cross_road = input("Your at a cross road . \n\t\t Choose Left or Right\n").lower()
+
+
+if cross_road in ["left","l"]:
+    print(logo.fort)
+    fort = input("You have reached the enemy fort  . Invade or Run ")
+
+elif cross_road in ['right','r']:
+    lake = input('You\'ve come to a lake. '
+                    'There is an island in the middle of the lake. '
+                    'Type "wait" to wait for a boat. '
+                    'Type "swim" to swim across.\n').lower()
+
+    if lake == "wait":
+        print(logo.island)
+        colour = input("You arrive at the island unharmed. "
+                        "There is house with 3 doors. One red, "
+                        "one yellow and one blue. "
+                        "Which colour do you choose?\n").lower()
+
+        if colour in ["red","r"]:
+            print("Congratulations ,you found the treasure chest.")
+
+        elif colour in ["yellow","y"]:
+            print("Game over , you walked into the sun planet and got burnt.")
+
+        elif colour in ["blue","b"]:
+            print("Hello")
+    else:
+        print(f"{logo.crocodile}\tYour were attacked by crocodiles and died . Game over ")
+else:
+    print(f"{logo.sloth}\nGame over , got bitten by a snake due to invalid input")
