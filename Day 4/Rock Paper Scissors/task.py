395c540bf9d87b194794e122de962ed63492dde2
Fungai023 <pmambambo023@student.wethinkcode.co.za>
2025-02-03 16:45:40 +0200

    Day 4 complete

--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -1,3 +1,6 @@
+import random
+from traceback import print_tb
+
 rock = '''
     _______
 ---'   ____)
@@ -24,3 +27,22 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
+game = [rock ,paper,scissors]
+
+user_input = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+print(game[user_input])
+computer = random.randint(0 , len(game)-1)
+print("Computer chose : \n" + game[computer])
+
+
+if user_input == computer :
+    print("It's a draw")
+elif (user_input == 0  and computer==1) or (user_input ==1 and computer ==2) or (user_input ==2 and computer ==0):
+    print("You loose")
+elif (user_input ==1  and computer==0) or (user_input ==2 and computer ==1) :
+    print("You win")
+elif 0 < user_input >= 3 :
+    print("Invalid input , you loose . Pls try again")
+
+
